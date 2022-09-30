24756296920afa14c36e557ff660cbf31b21ca3f
Anatoliy <tolxpams@gmail.com>
2022-09-30 17:26:44 +0300

    start vc

--- FindYourPath/Controller/StartVC/StartVC.swift
+++ FindYourPath/Controller/StartVC/StartVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartVC.swift
 //  FindYourPath
 //
 //  Created by Anatoliy Khramchenko on 30.09.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
